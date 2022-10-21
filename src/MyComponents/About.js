e1b7d2b4c9e5588b75cccf53c6cbb54c49b89cfc
Aaditya <aaaditya1314@gmail.com>
2022-10-21 09:36:38 +0530

    Inital Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h3>About</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facilis voluptate ducimus quas modi, saepe velit qui alias neque et cupiditate, dolorum recusandae hic explicabo molestias eius aut! Voluptate, aliquam.</p>
+        </div>
+    )
+}
